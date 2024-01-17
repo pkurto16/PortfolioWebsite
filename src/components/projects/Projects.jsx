f818b095f071a73f792386cb19d75d8e6ba4f4fc
pkurto16 <ptkurto3@gmail.com>
2024-01-16 22:03:10 -0500

    Updating Project Section Styling Etc.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,9 +8,9 @@ function Projects() {
             </h1>
             <div className="projects">
                 <ul className="displayed-projects">
-                    <div className="project">Project 1
-                    <a href={"https://riseforms.purduearc.com/"} > Purdue Arc Rise Form (Main Contributor to Styling)</a>
+                    <div className="project">Purdue Arc Website: A main contributor to styling and layout.
                     </div>
+                    <a className= "project-link" href= "https://riseforms.purduearc.com/" > Purdue Arc Rise Form (Main Contributor to Styling)</a>
                     <div className="project"> Project 2</div>
                     <div className="project"> Project 3</div>
                     <div className="project"> Project 4</div>
