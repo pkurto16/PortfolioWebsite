94cc9a539f4478258cbd6b4aafe286ba7320b0f7
pkurto16 <ptkurto3@gmail.com>
2024-01-16 21:56:19 -0500

    Added project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,7 +8,9 @@ function Projects() {
             </h1>
             <div className="projects">
                 <ul className="displayed-projects">
-                    <div className="project">Project 1</div>
+                    <div className="project">Project 1
+                    <a href={"https://riseforms.purduearc.com/"} > Purdue Arc Rise Form (Main Contributor to Styling)</a>
+                    </div>
                     <div className="project"> Project 2</div>
                     <div className="project"> Project 3</div>
                     <div className="project"> Project 4</div>
